816e7fb34322e536c6523b624dc75a569fc0c3b6
HimanshuSenjaliya <himanshusenjaliya11@gmail.com>
2024-03-26 10:33:39 +0530

    first commit

---
+++ types/index.ts
@@ -0,0 +1,60 @@
+
+import { manufacturers } from './../constants/index';
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps{
+    title:string;
+    btnType?: "button" | "submit";
+    containerStyles?:string;
+    handleClick ?: MouseEventHandler<HTMLButtonElement> ;
+    rightIcon ?: string  ; 
+    isDisabled ?: boolean ;
+}
+
+export interface SearchManufacturerProps{
+    manufacturer : string;
+    setManufacturer : (manufacturer : string) => void ;
+}
+
+export interface CarProps{
+    city_mpg:number;
+    class:string;
+    combination_mpg:number;
+    cylinders:number;
+    displacement:number;
+    drive:string;
+    fuel_type:string;
+    highway_mpg:number;
+    make:string;
+    model:string;
+    transmission:string;
+    year : number;
+}
+
+export interface FilterProps{
+    manufacturer:string ,
+    year: number,
+    fuel: string,
+    limit: number,
+    model:string ,
+}
+
+export interface HomeProps {
+    searchParams: FilterProps;
+}
+
+export interface OptionProps {
+    title :string ;
+    value : string;
+}
+
+
+export interface CustomFilterProps {
+    title : string;
+    options : OptionProps[]
+}
+
+export interface ShowMorePage{
+    pageNumber : number ;
+    isNext : boolean
+}
